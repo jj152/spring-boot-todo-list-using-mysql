c91023f2fb30652dbdba50e8e41b4db2f3255915
jj152 <ranadipdas.gcect@gmail.com>
2023-06-16 02:36:33 +0530

    initial commit

---
+++ src/main/java/com/example/springBootToDoList/SpringBootTodoListUsingMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoListUsingMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodoListUsingMysqlApplication.class, args);
+	}
+
+}
